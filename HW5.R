bf725a2c608bcb3fd22c04bb074209871d939fd7
unknown <tchott@CH215B-04.adit.mines.edu>
2016-04-21 15:37:31 -0600

    Work

--- HW5.R
+++ HW5.R
@@ -5,12 +5,18 @@ data(pipeline)
 head(pipeline)
 attach(pipeline)
 lmod1<-lm(Lab~Field)
-plot(lmod1$fitted, lmod1$residuals)
-abline(0,0)
+plot(lmod1) #gets you a plot of the residuals for question 1 and a qqplot of residuals
 
+#PART B
+i  <-order(pipeline$Field)
+npipe<-pipeline[i,]
+ff <- gl(12,9)[-108]
+meanfield <- unlist(lapply(split(npipe$Field,ff),mean))
+varlab <-unlist(lapply(split(npipe$Lab,ff),var))
 
+lmod2<-lm(log(varlab)~log(meanfield))
+summary(lmod2)
 
-
-
-
+lmod3<-lm(Lab~Field, weights=1/(Field)^1.12)
+plot(lmod3)
 
